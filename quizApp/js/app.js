a2b5bd38b54daa9b85907edec5b1ee04f611b365
roberto <robpalacios1@hotmail.com>
2021-04-19 21:36:24 -0500

    quiz app

---
+++ quizApp/js/app.js
@@ -0,0 +1,60 @@
+let questions = [{
+  question: "What is the baby of the Moth know as?",
+  choices:['baby', 'infant', 'kit', 'larva'],
+  correctAnswer: 3
+},
+{
+  question:"What is the adult of a kid called",
+  choices:['calf', 'doe', 'goat', 'chick'],
+  correctAnswer: 2
+},
+{
+  question:"What is the young of a bufallo called?",
+  choices:['calf', 'baby', 'pup', 'cow'],
+  correctAnswer: 0
+},
+{
+  question:"What a baby alligator called?",
+  choices:['baby', 'gator', 'hatchling', 'calf'],
+  correctAnswer: 2
+},
+{
+  question:"What is the baby Goose called?",
+  choices:['gooser', 'gosling', 'gup', 'pup'],
+  correctAnswer: 1
+},
+{
+  question:"What is the baby Hamster called",
+  choices:['pup', 'chick', 'infant', 'billy'],
+  correctAnswer: 0
+},
+{
+  question:"What is a baby Hawk called",
+  choices:['hawklett', 'pup', 'larva', 'eyeas'],
+  correctAnswer: 3
+},
+{
+  question:"What is a baby gasshopper called",
+  choices:['hopper', 'nymph', 'stick', 'pup'],
+  correctAnswer: 1
+},
+{
+  question:"What is a baby Kangaroo called",
+  choices:['kinga', 'joey', 'calf', 'baby'],
+  correctAnswer: 1
+},
+{
+  question:"What is a baby Whale called",
+  choices:['whala', 'cub', 'grub', 'infant'],
+  correctAnswer: 1
+},
+{
+  question:"What is a baby Monkey called",
+  choices:['infant', 'baby', 'calf', 'pup'],
+  correctAnswer: 0
+},
+{
+  question:"What is a baby Bear called",
+  choices:['cub', 'baby balu', 'young bear', 'bearlet'],
+  correctAnswer: 2
+}];
